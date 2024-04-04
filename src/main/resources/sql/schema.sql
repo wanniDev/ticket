59f9ac79caca8486c8a22e3a716fcb8df19b10e7
Wanni <wannidev0928@gmail.com>
2024-04-04 17:16:21 +0900

    Feat: erd를 바탕으로 스키마를 구성할 ddl 쿼리문 세팅

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,68 @@
+CREATE TABLE `user` (
+	`id`	varchar(255)    NOT NULL	COMMENT '사용자 식별자',
+	`username`	varchar(255)	NULL	COMMENT '사용자 이름',
+	`balance`	bigint	NULL,
+	`create_time`	datetime	NULL,
+	`update_time`	datetime	NOT NULL
+);
+
+CREATE TABLE `token` (
+	`id`	varchar(255)	NOT NULL,
+	`expiration_itme`	datetime	NULL,
+	`status`	enum('ACTIVE', 'EXPIRED')	NULL	COMMENT 'ACTIVE, EXPIRED',
+	`create_time`	datetime	NULL,
+	`user_id`	varchar(255)	NOT NULL	COMMENT '사용자 식별자'
+);
+
+CREATE TABLE `concert_date` (
+	`id`	varchar(255)	NOT NULL,
+	`date`	datetime	NULL
+);
+
+CREATE TABLE `seat` (
+	`id`	varchar(255)	NOT NULL,
+	`number`	int4	NULL,
+	`status`	enum('AVAILABLE', 'RESERVED', 'OCCUPIED')	NULL,
+	`id2`	varchar(255)	NOT NULL
+);
+
+CREATE TABLE `reservation` (
+	`id`	varchar(255)	NOT NULL,
+	`status`	enum('PENDING', 'RESERVED', 'OCCUPIED')	NULL,
+	`reservation_time`	datetime	NULL,
+	`expiration_time`	datetime	NULL,
+	`user_id`	varchar(255)	NOT NULL	COMMENT '사용자 식별자',
+	`concert_date_id`	varchar(255)	NOT NULL
+);
+
+CREATE TABLE `payment` (
+	`id`	varchar(255)	NOT NULL,
+	`amount`	bigint	NULL,
+	`payment_time`	datetime	NULL,
+	`reservation_id`	varchar(255)	NOT NULL,
+	`user_id`	varchar(255)	NOT NULL	COMMENT '사용자 식별자'
+);
+
+ALTER TABLE `user` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `token` ADD CONSTRAINT `PK_TOKEN` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `concert_date` ADD CONSTRAINT `PK_CONCERT_DATE` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `seat` ADD CONSTRAINT `PK_SEAT` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `reservation` ADD CONSTRAINT `PK_RESERVATION` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `payment` ADD CONSTRAINT `PK_PAYMENT` PRIMARY KEY (
+	`id`
+);
