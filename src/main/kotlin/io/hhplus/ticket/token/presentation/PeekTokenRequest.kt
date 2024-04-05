fd23ec23d34d4f9caabd654466dcafd5f8fcba10
Wanni <wannidev0928@gmail.com>
2024-04-05 17:02:46 +0900

    Feat: 클라이언트에서 polling을 수행할때 사용될 대기열 순번 확인 mock api 추가

---
+++ src/main/kotlin/io/hhplus/ticket/token/presentation/PeekTokenRequest.kt
@@ -0,0 +1,5 @@
+package io.hhplus.ticket.token.presentation
+
+data class PeekTokenRequest(
+    val tokenId: String
+)
--- src/main/kotlin/io/hhplus/ticket/token/presentation/TokenApi.kt
+++ src/main/kotlin/io/hhplus/ticket/token/presentation/TokenApi.kt
@@ -1,5 +1,6 @@
 package io.hhplus.ticket.token.presentation
 
+import io.hhplus.ticket.token.domain.Token
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,5 +15,11 @@ class TokenApi {
     fun createToken(@RequestBody request: CreateTokenRequest): ResponseEntity<CreateTokenResponse> {
         return ResponseEntity
             .ok(CreateTokenResponse("tokenId", LocalDateTime.now().plusMinutes(30L)))
+    }
+
+    @PostMapping("/peek")
+    fun peekToken(@RequestBody request: PeekTokenRequest): ResponseEntity<PeekTokenResponse> {
+        return ResponseEntity
+            .ok(PeekTokenResponse(true, LocalDateTime.now()))
     }
 }
