fd1cf6274ad3d9ec62945bda5615af5a1c22a09b
Wanni <wannidev0928@gmail.com>
2024-04-05 17:38:25 +0900

    Comment: 기능 설계 요약을 수행하기위해 결제 도메인 mockApi 로직 주석 추가

--- src/main/kotlin/io/hhplus/ticket/payment/presentation/PaymentApi.kt
+++ src/main/kotlin/io/hhplus/ticket/payment/presentation/PaymentApi.kt
@@ -12,16 +12,37 @@ import java.math.BigDecimal
 class PaymentApi {
     @PostMapping("/recharge")
     fun recharge(@RequestBody request: RechargeRequest): ResponseEntity<RechargeResponse> {
+        /**
+         * 1. 요청 입력값 검증
+         * 2. 사용자 id 검증
+         * 3. 결제 승인 호출
+         * 4. 사용자 잔액 업데이트
+         * 5. 응답
+         * 6. 로깅
+         */
         return ResponseEntity.ok(RechargeResponse("userId", request.amount))
     }
 
     @GetMapping("/balance")
     fun getBalance(@RequestParam userId: String): ResponseEntity<BalanceResponse> {
+        /**
+         * 1. 사용자 id 검증
+         * 2. 사용자 잔액 조회
+         * 3. 응답
+         */
         return ResponseEntity.ok(BalanceResponse("userId", BigDecimal(10000)))
     }
 
     @PostMapping
     fun pay(@RequestBody request: PayRequest): ResponseEntity<Void> {
+        /**
+         * 1. 요청 입력값 검증
+         * 2. 사용자 id 검증
+         * 3. 예약 id 검증
+         * 4. 결제 승인 호출
+         * 5. 예약 상태 업데이트
+         * 6. 응답
+         */
         return ResponseEntity.ok().build()
     }
 }
