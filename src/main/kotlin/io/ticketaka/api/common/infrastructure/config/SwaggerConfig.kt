13bdad9632e59b97e65c4ba0e8f837079cb69aee
Wanni <wannidev0928@gmail.com>
2024-04-08 22:44:18 +0900

    Chore: swagger 설정 추가

---
+++ src/main/kotlin/io/ticketaka/api/common/infrastructure/config/SwaggerConfig.kt
@@ -0,0 +1,35 @@
+package io.ticketaka.api.common.infrastructure.config
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@OpenAPIDefinition(
+    info = io.swagger.v3.oas.annotations.info.Info(
+        title = "tiketaka",
+        version = "0.0.1",
+        description = "tiketaka API specification"
+    )
+)
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI {
+        val securityScheme = SecurityScheme()
+            .name("bearer-jwt")
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+        val securityRequirement = SecurityRequirement().addList("bearer-jwt")
+        val components = Components()
+            .addSecuritySchemes("bearer-jwt", securityScheme)
+
+        return OpenAPI()
+            .addSecurityItem(securityRequirement)
+            .components(components)
+    }
+}
