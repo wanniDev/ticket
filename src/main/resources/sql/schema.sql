be94f3b05234cbf6f6c36f2af062022491f56910
Wanni <wannidev0928@gmail.com>
2024-04-10 00:07:12 +0900

    Refactor: 스키마 쿼리문 정리

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,121 +1,88 @@
-CREATE TABLE `user` (
-    `id`	bigint    NOT NULL,
+CREATE TABLE `users` (
+     `id` bigint auto_increment primary key,
     `tsid` varchar(255) NOT NULL UNIQUE,
     `point_id`	bigint	NOT NULL,
     INDEX user_tsid_idx (tsid)
 );
 
 CREATE TABLE `point` (
-	`id`	bigint    NOT NULL,
-    `tsid` varchar(255) NOT NULL UNIQUE,
-	`balance`   DECIMAL(19, 4)	NULL,
-    `create_time`	datetime	NOT NULL,
-    `update_time`	datetime	NULL,
+     `id` bigint auto_increment primary key,
+    `tsid` varchar(255) not null unique ,
+	`balance`   decimal(19, 4)	null,
+    `create_time`	datetime not null ,
+    `update_time`	datetime null,
     INDEX point_tsid_idx (tsid)
 );
 
 CREATE TABLE `token` (
-	`id`	bigint	NOT NULL,
-    `tsid` varchar(255) NOT NULL UNIQUE,
-	`issued_time`	datetime	NULL,
-	`status`	varchar(255)	NULL,
-	`user_tsid`	varchar(255)	NOT NULL,
+     `id` bigint auto_increment primary key,
+    `tsid` varchar(255) not null unique,
+	`issued_time`	datetime null,
+	`status`	varchar(255) null,
+	`user_tsid`	varchar(255) not null,
     INDEX token_tsid_idx (tsid)
 );
 
 CREATE TABLE `concert_date` (
-	`id`	bigint	NOT NULL,
-    `tsid` varchar(255) NOT NULL UNIQUE,
-	`date`	datetime	NULL,
+    `id` bigint auto_increment primary key,
+    `tsid` varchar(255) not null unique,
+	`date`	datetime null,
     INDEX concert_date_tsid_idx (tsid)
 );
 
 CREATE TABLE `seat` (
-	`id`	bigint	NOT NULL,
-    `tsid` varchar(255) NOT NULL UNIQUE,
-	`number`	varchar(255)	NULL,
-	`status`	varchar(255)	NULL,
-	`concert_date_id`	bigint	NOT NULL,
+    `id` bigint auto_increment primary key,
+    `tsid` varchar(255) not null unique,
+	`number`	varchar(255) null,
+	`status`	varchar(255) null ,
+	`concert_date_id`	bigint not null,
     INDEX seat_tsid_idx (tsid)
 );
 
 CREATE TABLE `reservation` (
-	`id`	bigint	NOT NULL,
-    `tsid` varchar(255) NOT NULL UNIQUE,
-    `status`	varchar(255)	NULL,
-	`reservation_time`	datetime	NULL,
-	`expiration_time`	datetime	NULL,
-	`user_id`	bigint	NOT NULL,
-	`seat_id`	bigint	NOT NULL,
+    `id` bigint auto_increment primary key,
+    `tsid` varchar(255) not null unique,
+    `status`	varchar(255) null,
+	`reservation_time`	datetime null,
+	`expiration_time`	datetime null,
+	`user_id`	bigint not null,
+	`seat_id`	bigint not null,
     INDEX reservation_tsid_idx (tsid)
 );
 
 CREATE TABLE `payment` (
-	`id`	bigint	NOT NULL,
-    `tsid` varchar(255) NOT NULL UNIQUE,
-	`amount`	DECIMAL(19, 4)	NULL,
-	`payment_time`	datetime	NULL,
-	`reservation_id`	bigint	NOT NULL,
-	`point_id`	bigint	NOT NULL,
+    `id` bigint auto_increment primary key,
+    `tsid` varchar(255) not null unique,
+	`amount`	DECIMAL(19, 4)	null,
+	`payment_time`	datetime null,
+	`reservation_id`	bigint	not null,
+	`point_id`	bigint not null,
     INDEX payment_tsid_idx (tsid)
 );
 
 CREATE TABLE `point_usage` (
-    `id`	bigint	NOT NULL,
-    `tsid` varchar(255) NOT NULL UNIQUE,
-    `transaction_type` varchar(255) NULL,
-    `user_id`	bigint	NOT NULL,
-    `point_id`	bigint	NOT NULL,
-    `create_time`	datetime	NOT NULL,
-    `update_time`	datetime	NULL,
+    `id` bigint auto_increment primary key,
+    `tsid` varchar(255) not null unique,
+    `transaction_type` varchar(255) null,
+    `user_id`	bigint	not null,
+    `point_id`	bigint	not null,
+    `create_time`	datetime not null,
+    `update_time`	datetime not null,
     INDEX point_usage_tsid_idx (tsid)
 );
 
 CREATE TABLE `payment_usage`(
-    `id`   bigint       NOT NULL,
-    `tsid` varchar(255) NOT NULL UNIQUE,
-    `transaction_key` varchar(255) NULL,
-    `amount` DECIMAL(19, 4) NULL,
-    `status` varchar(255) NULL,
-    `create_time`	datetime	NOT NULL,
-    `update_time`	datetime	NULL,
-    `payment_id` bigint NOT NULL,
-    `point_id` bigint NOT NULL,
-    INDEX upayment_usage_tsid_idx (tsid)
+    `id` bigint auto_increment primary key,
+    `tsid` varchar(255) not null unique,
+    `transaction_key` varchar(255) null,
+    `amount` DECIMAL(19, 4) null,
+    `status` varchar(255) null,
+    `create_time`	datetime not null,
+    `update_time`	datetime null,
+    `payment_id` bigint not null,
+    `point_id` bigint not null,
+    INDEX payment_usage_tsid_idx (tsid)
 );
 
-ALTER TABLE `user` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
-    `id`
-);
-
-ALTER TABLE `point` ADD CONSTRAINT `PK_POINT` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `token` ADD CONSTRAINT `PK_TOKEN` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `concert_date` ADD CONSTRAINT `PK_CONCERT_DATE` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `seat` ADD CONSTRAINT `PK_SEAT` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `reservation` ADD CONSTRAINT `PK_RESERVATION` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `payment` ADD CONSTRAINT `PK_PAYMENT` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `point_usage` ADD CONSTRAINT `PK_USER_POINT` PRIMARY KEY (
-    `id`
-);
-
-ALTER TABLE `payment_usage` ADD CONSTRAINT `PK_PAYMENT_USAGE` PRIMARY KEY (
-    `id`
-);
+insert into `point` (`id`, `tsid`, `balance`, `create_time`, `update_time`) values (1, 'point1', 1000, '2024-01-01 00:00:00', '2024-01-01 00:00:00');
+insert into `users` (`id`, `tsid`, `point_id`) values (1, 'user1', 1);
