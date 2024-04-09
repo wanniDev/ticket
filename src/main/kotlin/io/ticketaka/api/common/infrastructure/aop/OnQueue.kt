a023a6804a38791a8344a2692d3291cc3d948bf4
Wanni <wannidev0928@gmail.com>
2024-04-09 22:47:01 +0900

    Feat: 토큰 대기열을 활용할 api에 적용시킬 aop 구현

---
+++ src/main/kotlin/io/ticketaka/api/common/infrastructure/aop/OnQueue.kt
@@ -0,0 +1,9 @@
+package io.ticketaka.api.common.infrastructure.aop
+
+import org.springframework.stereotype.Indexed
+
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@Indexed
+annotation class OnQueue()
---
+++ src/main/kotlin/io/ticketaka/api/common/infrastructure/aop/RequestQueueAspect.kt
@@ -0,0 +1,26 @@
+package io.ticketaka.api.common.infrastructure.aop
+
+import io.ticketaka.api.common.domain.queue.TokenWaitingQueue
+import io.ticketaka.api.token.domain.Token
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class RequestQueueAspect(
+    private val tokenWaitingQueue: TokenWaitingQueue
+) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @Pointcut("@annotation(io.ticketaka.api.common.infrastructure.aop.OnQueue) || @within(io.ticketaka.api.common.infrastructure.aop.OnQueue)")
+    fun onQueuePointcut() {
+    }
+
+    @Before("onQueuePointcut()")
+    fun beforeOnQueueAdvice() {
+        logger.info("RequestQueueAspect")
+    }
+}
