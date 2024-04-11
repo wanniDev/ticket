1d8701113a4723952951872aa877206e047d8a42
Wanni <wannidev0928@gmail.com>
2024-04-12 06:00:59 +0900

    Refactor: 대기열과 예약로직에 집중하기 위해 불필요한 Dummy PG 기능 삭제

--- src/main/kotlin/io/ticketaka/api/reservation/presentation/dto/RechargeRequest.kt
+++
@@ -1,8 +0,0 @@
-package io.ticketaka.api.reservation.presentation.dto
-
-import java.math.BigDecimal
-
-data class RechargeRequest(
-    val userId: String,
-    val amount: BigDecimal
-)
--- src/main/kotlin/io/ticketaka/api/reservation/presentation/dto/RechargeResponse.kt
+++
@@ -1,8 +0,0 @@
-package io.ticketaka.api.reservation.presentation.dto
-
-import java.math.BigDecimal
-
-data class RechargeResponse(
-    val userId: String,
-    val newBalance: BigDecimal
-)
