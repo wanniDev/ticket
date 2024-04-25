7fb8551265de49ec02c74d83e7bd89cf48bc289e
Wanni <wannidev0928@gmail.com>
2024-04-25 14:10:53 +0900

    Chore: 올바른 코틀린  관련 컨벤션 작성을 위해 ktlint 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
     kotlin("plugin.jpa") version "1.9.23"
