714391f94350d0ee98161d037b40cef5a5a54273
Wanni <wannidev0928@gmail.com>
2024-05-02 23:44:24 +0900

    Comment: JWT를 적용할 준비가 안되어있음. 일단 해당 필터 비활성화

--- src/main/kotlin/io/ticketaka/api/common/infrastructure/jwt/JwtFilter.kt
+++ src/main/kotlin/io/ticketaka/api/common/infrastructure/jwt/JwtFilter.kt
@@ -5,13 +5,11 @@ import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
-import org.springframework.core.annotation.Order
-import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
 
-@Component
-@Order(1)
+// @Component
+// @Order(1)
 class JwtFilter(
     private val jwtTokenParser: JwtTokenParser,
 ) : OncePerRequestFilter() {
