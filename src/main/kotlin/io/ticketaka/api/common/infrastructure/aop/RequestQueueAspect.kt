410ddb5c39f1a887ba9c49ff01351c94acf92ee0
Wanni <wannidev0928@gmail.com>
2024-05-04 00:01:27 +0900

    Feat: 대기열 토큰 검증 aop 구현
    - 현재 순번의 토큰과 클라이언트가 지닌 토큰이 일치한지 검증한다. 만약 틀렸다면, 예외가 발생하고 맞으면 트랜잭션 수행 후 해당 토큰은 삭제한다.
    - DB 대기열을 너무 많이 활용하는 것도 성능 저하의 원인이 될 수 있기 때문에 우선 트래픽이 많이 몰릴 확률이 높은 예약 신청, 확정 api에만 큐 대기열을 걸어놓는다.

--- src/main/kotlin/io/ticketaka/api/common/infrastructure/aop/RequestQueueAspect.kt
+++ src/main/kotlin/io/ticketaka/api/common/infrastructure/aop/RequestQueueAspect.kt
@@ -1,15 +1,20 @@
 package io.ticketaka.api.common.infrastructure.aop
 
 import io.ticketaka.api.common.domain.queue.TokenWaitingQueue
+import io.ticketaka.api.common.infrastructure.jwt.JwtTokenParser
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
-import org.aspectj.lang.annotation.Before
 import org.aspectj.lang.annotation.Pointcut
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
 
 @Aspect
 @Component
 class RequestQueueAspect(
+    private val jwtTokenParser: JwtTokenParser,
     private val tokenWaitingQueue: TokenWaitingQueue,
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
@@ -20,11 +25,24 @@ class RequestQueueAspect(
     fun onQueuePointcut() {
     }
 
-    @Before("onQueuePointcut()")
-    fun beforeOnQueueAdvice() {
-        logger.info("RequestQueueAspect")
-        tokenWaitingQueue.peek()?.let {
-            logger.info("Token: ${it.tsid}")
+    @Around("onQueuePointcut()")
+    fun beforeOnQueueAdvice(joinPoint: ProceedingJoinPoint) {
+        val requestAttributes = RequestContextHolder.getRequestAttributes() as ServletRequestAttributes?
+        val authorizationHeader =
+            requestAttributes?.request?.getHeader(
+                "tokenTsid",
+            ) ?: throw IllegalArgumentException("Authorization header is not found")
+
+        val firstToken = tokenWaitingQueue.peek()
+
+        if (firstToken == null || firstToken.tsid != authorizationHeader) {
+            val errorMessage = "현재 대기 중인 토큰과 요청한 토큰이 일치하지 않습니다."
+            logger.error(errorMessage)
+            throw IllegalArgumentException(errorMessage)
         }
+
+        joinPoint.proceed()
+
+        tokenWaitingQueue.poll()
     }
 }
