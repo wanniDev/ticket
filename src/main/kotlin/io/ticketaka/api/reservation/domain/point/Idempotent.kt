4a229190fe8054bd23ff5364c72259363e498baf
Wanni <wannidev0928@gmail.com>
2024-05-07 00:23:50 +0900

    Fix: lint 규약 위반한 코드(import 구문에 와일드 카드 사용한 부분) 정리

--- src/main/kotlin/io/ticketaka/api/reservation/domain/point/Idempotent.kt
+++ src/main/kotlin/io/ticketaka/api/reservation/domain/point/Idempotent.kt
@@ -7,7 +7,6 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.math.BigDecimal
-import java.util.*
 
 @Entity
 @Table(name = "idempotent")
