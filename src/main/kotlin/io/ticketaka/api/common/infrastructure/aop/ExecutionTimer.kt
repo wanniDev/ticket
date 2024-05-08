79b7871424f04d492111b88aca0f4045a28f12df
Wanni <wannidev0928@gmail.com>
2024-05-08 20:01:58 +0900

    Feat: 쿼리 튜닝후, DB 트랜잭션 실행시간 측정하기위한 aop 구현

---
+++ src/main/kotlin/io/ticketaka/api/common/infrastructure/aop/ExecutionTimer.kt
@@ -0,0 +1,34 @@
+package io.ticketaka.api.common.infrastructure.aop
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+import org.springframework.util.StopWatch
+
+@Aspect
+@Component
+@Profile("dev")
+class ExecutionTimer {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @Pointcut(
+        "@annotation(org.springframework.web.bind.annotation.GetMapping) || @annotation(org.springframework.web.bind.annotation.PostMapping)",
+    )
+    fun timerPointcut() {
+    }
+
+    @Around("timerPointcut()")
+    fun assumeExecutionTime(joinPoint: ProceedingJoinPoint) {
+        val stopWatch = StopWatch()
+
+        stopWatch.start()
+        joinPoint.proceed()
+        stopWatch.stop()
+
+        logger.info("Execution time: ${stopWatch.totalTimeMillis} ms")
+    }
+}
