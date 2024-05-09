08e059560d6007258e1cdde90b53f95e302e4865
Wanni <wannidev0928@gmail.com>
2024-05-09 23:50:33 +0900

    Refactor: entityGraph를 활용하여, 사용자 조회시, point를 join 쿼리로 변경
    어차피, 테이블간의 fk 제약이 없기 때문에 의존되는 테이블의 레코드 존재 유무를 확인하기위해 따로 쿼리를 구성해야한다. 그럴바에 단일 select 두개를 join 하나로 줄인다.
    쿼리 횟수 5개에서 4개로 축소

--- src/main/kotlin/io/ticketaka/api/user/infrastructure/jpa/JpaUserRepository.kt
+++ src/main/kotlin/io/ticketaka/api/user/infrastructure/jpa/JpaUserRepository.kt
@@ -1,8 +1,10 @@
 package io.ticketaka.api.user.infrastructure.jpa
 
 import io.ticketaka.api.user.domain.User
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface JpaUserRepository : JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = ["point"])
     fun findByTsid(userTsid: String): User?
 }
