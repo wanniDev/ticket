179da91ffaeabe1863e92ff78c06c0d0ecaa7a3e
Wanni <wannidev0928@gmail.com>
2024-05-17 18:11:11 +0900

    Feat: 포인트 도메인을 예약 도메인으로부터 고립 시키기 위해, 결제 후 사용자의 포인트 잔액 수정하는 작업과 관련된 orm 함수 작성

--- src/main/kotlin/io/ticketaka/api/reservation/domain/point/PointRepository.kt
+++ src/main/kotlin/io/ticketaka/api/reservation/domain/point/PointRepository.kt
@@ -1,9 +1,16 @@
 package io.ticketaka.api.reservation.domain.point
 
+import java.math.BigDecimal
+
 interface PointRepository {
     fun save(point: Point): Point
 
     fun findByTsid(tsid: String): Point?
 
     fun findById(pointId: Long): Point?
+
+    fun updateBalance(
+        pointId: Long,
+        balance: BigDecimal,
+    )
 }
--- src/main/kotlin/io/ticketaka/api/reservation/infrastructure/persistence/PointRepositoryComposition.kt
+++ src/main/kotlin/io/ticketaka/api/reservation/infrastructure/persistence/PointRepositoryComposition.kt
@@ -4,6 +4,7 @@ import io.ticketaka.api.reservation.domain.point.Point
 import io.ticketaka.api.reservation.domain.point.PointRepository
 import io.ticketaka.api.reservation.infrastructure.jpa.JpaPointRepository
 import org.springframework.stereotype.Repository
+import java.math.BigDecimal
 
 @Repository
 class PointRepositoryComposition(
@@ -19,5 +20,12 @@ class PointRepositoryComposition(
 
     override fun findById(pointId: Long): Point? {
         return jpaPointRepository.findById(pointId).orElse(null)
+    }
+
+    override fun updateBalance(
+        pointId: Long,
+        balance: BigDecimal,
+    ) {
+        jpaPointRepository.updateBalanceById(pointId, balance)
     }
 }
