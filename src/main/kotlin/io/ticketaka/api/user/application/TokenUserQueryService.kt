93cb932e86ea33a512b30c62ea062252e2ba6d6d
Wanni <wannidev0928@gmail.com>
2024-05-21 22:57:17 +0900

    Refactor: 성능 개선을 위해 캐시가 적용된 readonly 트랜잭션 비즈니스 로직은 트랜잭션 매니저의 관리를 받지 않기로함

--- src/main/kotlin/io/ticketaka/api/user/application/TokenUserQueryService.kt
+++ src/main/kotlin/io/ticketaka/api/user/application/TokenUserQueryService.kt
@@ -5,15 +5,12 @@ import io.ticketaka.api.user.domain.User
 import io.ticketaka.api.user.domain.UserRepository
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional(readOnly = true)
 class TokenUserQueryService(
     private val userRepository: UserRepository,
 ) {
     @Cacheable(value = ["user"], key = "#tsId")
-    @Transactional(readOnly = true)
     fun getUser(tsId: String): User {
         return userRepository.findByTsid(tsId) ?: throw NotFoundException("사용자를 찾을 수 없습니다.")
     }
