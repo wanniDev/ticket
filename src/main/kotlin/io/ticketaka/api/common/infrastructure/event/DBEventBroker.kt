6ad8b388f6c862bda5da85aedb9e5d4be41ab4ae
Wanni <wannidev0928@gmail.com>
2024-05-31 20:13:41 +0900

    Feat: Lazy write를 통해 서버의 부하를 분산시키기 위해, DB 기반의 이벤트 브로커 구현

---
+++ src/main/kotlin/io/ticketaka/api/common/infrastructure/event/DBEventBroker.kt
@@ -0,0 +1,39 @@
+package io.ticketaka.api.common.infrastructure.event
+
+import io.ticketaka.api.common.domain.DomainEvent
+import io.ticketaka.api.common.domain.EventBroker
+import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentLinkedQueue
+import kotlin.concurrent.thread
+
+@Component
+class DBEventBroker(
+    private val eventDispatcher: EventDispatcher,
+) : EventBroker {
+    private val eventQueue = ConcurrentLinkedQueue<DomainEvent>()
+
+    init {
+        startEventBroker()
+    }
+
+    override fun produce(domainEvent: DomainEvent) {
+        eventQueue.add(domainEvent)
+    }
+
+    private fun startEventBroker() {
+        thread(start = true) {
+            while (true) {
+                if (eventQueue.isNotEmpty()) {
+                    val events = mutableListOf<DomainEvent>()
+                    while (eventQueue.isNotEmpty()) {
+                        eventQueue.poll()?.let { events.add(it) }
+                    }
+                    eventDispatcher.dispatch(events)
+                } else {
+                    Thread.sleep(5000)
+                    continue
+                }
+            }
+        }
+    }
+}
