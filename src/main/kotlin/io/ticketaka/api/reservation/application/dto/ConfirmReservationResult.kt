bc448299fca2ee635674c796f3153e27990f1645
Wanni <wannidev0928@gmail.com>
2024-06-06 20:21:01 +0900

    Refactor: 예약 도메인 리팩토링 과정에서 사용하지 않게된 dto 클래스 삭제

--- src/main/kotlin/io/ticketaka/api/reservation/application/dto/ConfirmReservationResult.kt
+++
@@ -1,8 +0,0 @@
-package io.ticketaka.api.reservation.application.dto
-
-import io.ticketaka.api.reservation.domain.reservation.Reservation
-
-data class ConfirmReservationResult(
-    val reservationTsid: String,
-    val status: Reservation.Status,
-)
