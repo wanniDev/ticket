b2715528a99fa956df95fce22fe509b6d23aa26a
Wanni <wannidev0928@gmail.com>
2024-06-06 20:14:34 +0900

    Refactor: 포인트 결제와 pg 결제 승인(mock) 분리이후 사용하지 않는 함수 삭제

--- src/main/kotlin/io/ticketaka/api/point/application/PaymentService.kt
+++ src/main/kotlin/io/ticketaka/api/point/application/PaymentService.kt
@@ -3,35 +3,17 @@ package io.ticketaka.api.point.application
 import io.ticketaka.api.point.application.dto.PaymentCommand
 import io.ticketaka.api.point.domain.payment.Payment
 import io.ticketaka.api.point.domain.payment.PaymentRepository
-import org.springframework.context.ApplicationEventPublisher
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional(readOnly = true)
 class PaymentService(
     private val paymentRepository: PaymentRepository,
-    private val pointCacheAsideQueryService: PointCacheAsideQueryService,
-    private val applicationEventPublisher: ApplicationEventPublisher,
 ) {
     @Transactional
     fun paymentApproval(paymentCommand: PaymentCommand) {
         Thread.sleep((500..1000).random().toLong()) // PG 승인 요청 시간 대기
         paymentRepository.save(Payment.newInstance(paymentCommand.amount, paymentCommand.userId, paymentCommand.pointId))
-    }
-
-    @Async
-    @Transactional(propagation = Propagation.NESTED)
-    fun paymentApprovalAsync(paymentCommand: PaymentCommand) {
-        try {
-            Thread.sleep((500..1000).random().toLong()) // PG 승인 요청 시간 대기
-            val payment = Payment.newInstance(paymentCommand.amount, paymentCommand.userId, paymentCommand.pointId)
-            paymentRepository.save(payment)
-            payment.pollAllEvents().forEach { applicationEventPublisher.publishEvent(it) }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
     }
 }
