0f836d008172d1f733597d793114e658b930f409
choi hyung joong <81374655+wanniDev@users.noreply.github.com>
2024-06-13 00:57:27 +0900

    Feat: eventually consistency로 DB write를 수행할 경우 생기는 문제를 대응하기 위한 포인트 충전, 결제 코드에 eventLogger 구현 (#21)

--- src/main/kotlin/io/ticketaka/api/point/domain/PointHistory.kt
+++ src/main/kotlin/io/ticketaka/api/point/domain/PointHistory.kt
@@ -63,4 +63,24 @@ class PointHistory(
             )
         }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PointHistory
+
+        if (id != other.id) return false
+        if (userId != other.userId) return false
+        if (pointId != other.pointId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + userId.hashCode()
+        result = 31 * result + pointId.hashCode()
+        return result
+    }
 }
--- src/main/kotlin/io/ticketaka/api/point/infrastructure/event/PointChargeEventConsumer.kt
+++ src/main/kotlin/io/ticketaka/api/point/infrastructure/event/PointChargeEventConsumer.kt
@@ -1,17 +1,24 @@
 package io.ticketaka.api.point.infrastructure.event
 
+import io.ticketaka.api.point.domain.DBPointManager
 import io.ticketaka.api.point.domain.PointChargeEvent
 import io.ticketaka.api.point.domain.PointHistory
 import io.ticketaka.api.point.domain.PointHistoryRepository
 import org.springframework.stereotype.Component
-import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.LinkedBlockingQueue
 import kotlin.concurrent.thread
 
 @Component
 class PointChargeEventConsumer(
     private val pointHistoryRepository: PointHistoryRepository,
+    private val dbPointManger: DBPointManager,
+    private val asyncEventLogAppender: AsyncEventLogAppender,
 ) {
-    private val eventQueue = ConcurrentLinkedQueue<PointChargeEvent>()
+    private val eventQueue = LinkedBlockingQueue<PointChargeEvent>()
+    private val maxRetries = 3
+    private val warningForRetry = "Retry on failure."
+    private val retryFailed = "Retry failed."
+    private val warningForOffer = "Offer failed."
 
     init {
         startEventConsumer()
@@ -20,18 +27,40 @@ class PointChargeEventConsumer(
     fun consume(events: MutableList<PointChargeEvent>) {
         val pointHistories = mutableListOf<PointHistory>()
         events.forEach { event ->
+            asyncEventLogAppender.appendInfo(event)
+
             PointHistory.newInstance(
                 userId = event.userId,
                 pointId = event.pointId,
                 amount = event.amount,
                 transactionType = PointHistory.TransactionType.CHARGE,
             ).let { pointHistories.add(it) }
+
+            retryOnFailure(event)
         }
         pointHistoryRepository.saveAll(pointHistories)
     }
 
+    private fun retryOnFailure(
+        event: PointChargeEvent,
+        retryCount: Int = 0,
+    ) {
+        try {
+            dbPointManger.charge(event)
+        } catch (e: Exception) {
+            if (retryCount < maxRetries) {
+                asyncEventLogAppender.appendWarning(event, warningForRetry)
+                retryOnFailure(event, retryCount + 1)
+            } else {
+                asyncEventLogAppender.appendError(event, retryFailed)
+            }
+        }
+    }
+
     fun offer(event: PointChargeEvent) {
-        eventQueue.add(event)
+        if (!eventQueue.offer(event, 1000, java.util.concurrent.TimeUnit.MILLISECONDS)) {
+            asyncEventLogAppender.appendError(event, warningForOffer)
+        }
     }
 
     private fun startEventConsumer() {
