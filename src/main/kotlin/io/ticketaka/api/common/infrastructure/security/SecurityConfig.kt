b9a60f198341b3edb2b974853432c7190a25c7a3
Wanni <wannidev0928@gmail.com>
2024-06-14 22:49:57 +0900

    Feat: Security가 적용된 이후 기본적으로 api 실행이 가능한 상태를 유지하기 위해, 우선은 apiEndpoint를 permitAll로 설정

---
+++ src/main/kotlin/io/ticketaka/api/common/infrastructure/security/SecurityConfig.kt
@@ -0,0 +1,24 @@
+package io.ticketaka.api.common.infrastructure.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableMethodSecurity
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { it.anyRequest().permitAll() } // TODO oauth 구현이후 endpoint별 권한 설정 필요
+            .formLogin { it.disable() }
+        return http.build()
+    }
+}
