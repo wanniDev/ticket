3f543f28abe96f84fe242ef7a0665c0bed1f6923
Wanni <wannidev0928@gmail.com>
2024-06-14 23:30:40 +0900

    Feat: Jwt 인증필터와 SpringSecurity의 충돌을 피하기위해, Jwt 인증 필터와 예외 필터, securityFilterChain에 추가

--- src/main/kotlin/io/ticketaka/api/common/infrastructure/security/SecurityConfig.kt
+++ src/main/kotlin/io/ticketaka/api/common/infrastructure/security/SecurityConfig.kt
@@ -1,5 +1,7 @@
 package io.ticketaka.api.common.infrastructure.security
 
+import io.ticketaka.api.user.infrastructure.jwt.JwtAuthenticationFilter
+import io.ticketaka.api.user.infrastructure.jwt.JwtExceptionFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
@@ -7,11 +9,34 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.ExceptionTranslationFilter
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity(debug = true)
 @EnableMethodSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtExceptionFilter: JwtExceptionFilter,
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+) {
+    /*
+     *
+Security filter chain: [
+  DisableEncodeUrlFilter
+  WebAsyncManagerIntegrationFilter
+  SecurityContextHolderFilter
+  HeaderWriterFilter
+  CorsFilter
+  LogoutFilter
+  RequestCacheAwareFilter
+  SecurityContextHolderAwareRequestFilter
+  AnonymousAuthenticationFilter
+  SessionManagementFilter
+  ExceptionTranslationFilter
+  AuthorizationFilter
+]
+
+     * */
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
@@ -19,6 +44,8 @@ class SecurityConfig {
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authorizeHttpRequests { it.anyRequest().permitAll() } // TODO oauth 구현이후 endpoint별 권한 설정 필요
             .formLogin { it.disable() }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterBefore(jwtExceptionFilter, ExceptionTranslationFilter::class.java)
         return http.build()
     }
 }
