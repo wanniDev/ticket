78fde27d42ccaabc43d12c03963a25adb408b64f
Wanni <wannidev0928@gmail.com>
2024-06-14 11:41:42 +0900

    Remove: JWT는 사용자 도메인에서 활용되므로 기존에 common에서 생성한 JWT 비즈니스 로직 및 구현체 삭제

--- src/main/kotlin/io/ticketaka/api/common/infrastructure/jwt/JwtTokens.kt
+++
@@ -1,6 +0,0 @@
-package io.ticketaka.api.common.infrastructure.jwt
-
-data class JwtTokens(
-    val accessToken: String,
-    val refreshToken: String,
-)
--- src/main/kotlin/io/ticketaka/api/common/infrastructure/jwt/RawToken.kt
+++
@@ -1,5 +0,0 @@
-package io.ticketaka.api.common.infrastructure.jwt
-
-data class RawToken(
-    val subject: String,
-)
