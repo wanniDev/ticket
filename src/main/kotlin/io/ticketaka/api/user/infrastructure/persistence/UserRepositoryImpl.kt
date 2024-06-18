9287b59f2264e960773728fd05544755968952eb
Wanni <wannidev0928@gmail.com>
2024-06-18 12:39:41 +0900

    Feat: 최초 소셜 로그인시 계정 등록에 필요한 orm 새로 구성

--- src/main/kotlin/io/ticketaka/api/user/infrastructure/persistence/UserRepositoryImpl.kt
+++ src/main/kotlin/io/ticketaka/api/user/infrastructure/persistence/UserRepositoryImpl.kt
@@ -6,8 +6,12 @@ import io.ticketaka.api.user.infrastructure.jpa.JpaUserRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-class UserRepositoryImpl(private val jpaUserRepository: JpaUserRepository) : UserRepository {
-    override fun findById(id: Long): User? {
-        return jpaUserRepository.findById(id).orElse(null)
-    }
+class UserRepositoryImpl(
+    private val jpaUserRepository: JpaUserRepository,
+) : UserRepository {
+    override fun save(user: User): User = jpaUserRepository.save(user)
+
+    override fun findById(id: Long): User? = jpaUserRepository.findById(id).orElse(null)
+
+    override fun findByEmail(email: String): User? = jpaUserRepository.findByEmail(email)
 }
