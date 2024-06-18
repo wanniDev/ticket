9f4f3bada6394286bc24794d78441bbc3ae3205d
Wanni <wannidev0928@gmail.com>
2024-06-18 11:09:34 +0900

    Chore: OAuth2 시연을 위해 타임리프 템플릿 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
+    // thymeleaf
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+
     // testcontainers
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
